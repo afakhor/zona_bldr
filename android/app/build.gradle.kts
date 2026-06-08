26daa20d070dd830ce2f4fdef26f365cda518b26
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-08 18:47:31 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.zona_bldr" // Ganti sesuai package kamu
+    namespace = "com.heruwngchn.addhmescrn"
     compileSdk = 34
     
     defaultConfig {
