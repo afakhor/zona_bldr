0a61dd6d3658998a9253343739770f37e41f256b
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-08 12:02:32 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,23 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// WAJIB ADA INI BUAT AGP 8 + Flutter 3.22
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register<Delete>("clean") {
